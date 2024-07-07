6f556b9be091946086b2196af0ef8f8c2e8f12ae
LuckyNoobz <r.abudzar@gmail.com>
2024-07-08 04:12:47 +0700

    Add API Endpoint

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,4 +22,12 @@ class PostController extends Controller
         $data = Post::find($id);
         return response()->json($data, 200);
     }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $response = Post::create($data);
+
+        return response()->json($response, 201);
+    }
 }
